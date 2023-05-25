69146ceeef6aa9dfb8b58e03fb9ff28fabd525a0
keenhydra <62450181+Keenhydra@users.noreply.github.com>
2023-05-25 22:28:48 +0200

    Add counter, first to 5, reload button

--- java.js
+++ java.js
@@ -1,5 +1,12 @@
 //JavaScript
+//get computerSelection every round
+//return score to document
+//create round counter XXX
+//create score keeper
+//return score/round to document
+//create big ol reset button
 
+//finish up bug fixes
 
 const rocks = document.getElementById("rock");
 rocks.addEventListener("click", myRock);
@@ -10,59 +17,102 @@ papers.addEventListener("click", myPaper);
 const scissors = document.getElementById("scissors");
 scissors.addEventListener("click", myScissor);
 
-
 const container = document.querySelector('.container-images');
+container.addEventListener('click', getComputerChoice)
 container.addEventListener('click', playRound)
+container.addEventListener('click', update)
+
 
 function myScissor() {
     playerSelection = 'scissors';
-    console.log(playerSelection);
     return playerSelection;
 }
 
 function myPaper() {
     playerSelection = 'paper';
-    console.log(playerSelection)
     return playerSelection;
 }
 
 function myRock() {
     playerSelection = 'rock';
-    console.log(playerSelection)
     return playerSelection;
 }
 
 function getComputerChoice(){
     const choice =['rock', 'paper', 'scissors']
     const random = Math.floor(Math.random() * choice.length);
-    return random, choice[random];
+    computerSelection = (random,choice[random]);
+    return computerSelection;
 }
 
-computerSelection = getComputerChoice();
+playerScore = 0;
+computerScore = 0;
 
 function playRound(){
         //PlayerSelection Rock
     if (playerSelection === 'rock' && computerSelection === 'scissors'){
-        return console.log('You Win!')
+        document.getElementById("result").innerText = "You Won!";
+        playerScore += 1;
     }else if (playerSelection === 'rock' && computerSelection === 'paper'){
-        return console.log('You Lose!')
+        document.getElementById("result").innerText = "You Lose!";
+        computerScore += 1;
     }else if(playerSelection === 'rock' && computerSelection === 'rock'){
-        return console.log('You Tied!')
+        document.getElementById("result").innerText = 'You Tied!';
         //PlayerSelection Paper
     }else if (playerSelection === 'paper' && computerSelection === 'scissors'){
-        return console.log('You Lose!')
+        document.getElementById("result").innerText = "You Lose!";
+        computerScore += 1;
     }else if (playerSelection === 'paper' && computerSelection === 'paper'){
-        return console.log('You Tied!')
+        document.getElementById("result").innerText = "You Tied!";
     }else if(playerSelection === 'paper' && computerSelection === 'rock'){
-        return console.log('You Win!')
+        document.getElementById("result").innerText = "You Won!";
+        playerScore += 1;
         //PlayerSelection Scissors
     }else if (playerSelection === 'scissors' && computerSelection === 'scissors'){
-        return console.log('You Tied!')
+        document.getElementById("result").innerText = "You Tied!";
     }else if (playerSelection === 'scissors' && computerSelection === 'paper'){
-        return console.log('You Win!')
+        document.getElementById("result").innerText = "You Won!";
+        playerScore += 1;
     }else if(playerSelection === 'scissors' && computerSelection === 'rock'){
-        return console.log('You Lose!')
+        document.getElementById("result").innerText = "You Lose!";
+        computerScore += 1;
     }else{
-        return "Invalid input! Can't compare " + (playerSelection) + ' to ' + (computerSelection)
+        return "Invalid input! Can't compare "
     }
   }
+
+
+function update() {
+    document.getElementById('score-player').innerText = `${playerScore}`
+    document.getElementById('score-computer').innerText = `${computerScore}`
+    document.getElementById('computer-selection').innerText = `Computer picked ${computerSelection}`
+    if (playerScore === 5){
+        document.getElementById('computer-selection').innerText = `You won the game!`;
+        removeAllChildNodes(parent);
+        parent.appendChild(btn);
+        const reload = document.getElementById('reload');
+        reload.addEventListener("click", refresh);
+    } else if (computerScore === 5){
+        document.getElementById('computer-selection').innerText = `Computer won the game!`;
+        removeAllChildNodes(parent);      
+        parent.appendChild(btn);
+        const reload = document.getElementById('reload');
+        reload.addEventListener("click", refresh);
+    } 
+    }
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+const parent = document.querySelector('.display-result');
+
+let btn = document.createElement("button");
+btn.innerHTML = "Reload";
+btn.id = 'reload'
+
+
+function refresh() {
+    location.reload();
+}
